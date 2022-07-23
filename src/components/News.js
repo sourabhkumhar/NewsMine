96610e240b423089848ca4ad1d45f037f6c0ef3c
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2022-07-23 12:28:32 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -48,12 +48,12 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    this.updateNews(0);
+    this.updateNews();
   }
 
   fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0341080f799a4e5b9438cb005d2e049c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0341080f799a4e5b9438cb005d2e049c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
